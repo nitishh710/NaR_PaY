5da0c29a882e938d6b263c5db51d6895a200efc5
nitishh710 <nitishchoudhary5585@gmail.com>
2026-05-23 07:18:00 +0530

    fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,61 @@
-import { Routes, Route } from "react-router-dom";
-import GetAmount from "./pages/GetAmount";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Transfer from "./pages/Transfer";
 import History from "./pages/History";
+import GetAmount from "./pages/GetAmount";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/transfer" element={<Transfer />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/get-amount" element={<GetAmount />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+
+        {/* DEFAULT ROUTE */}
+        <Route
+          path="/"
+          element={<Navigate to="/login" />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/transfer"
+          element={<Transfer />}
+        />
+
+        <Route
+          path="/history"
+          element={<History />}
+        />
+
+        <Route
+          path="/get-amount"
+          element={<GetAmount />}
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
