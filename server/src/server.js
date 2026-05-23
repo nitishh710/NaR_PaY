3578c4b8d598b5b0b881b04823169ef7ac274d0d
nitishh710 <nitishchoudhary5585@gmail.com>
2026-05-23 16:19:11 +0530

    final railway fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,17 @@
-import app from "./app.js";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
