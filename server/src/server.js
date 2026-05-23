bd6de9ce65116841903baac24e745138fe4b5a7f
nitishh710 <nitishchoudhary5585@gmail.com>
2026-05-23 16:16:08 +0530

    fixed railway port

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,20 @@
+import app from "./app.js";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-import app from "./app.js";
-import connectDB from "./config/db.js";
+const PORT = process.env.PORT || 8080;
 
-connectDB();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
 
-const PORT = process.env.PORT || 5000;
-
-console.log(
-  "EMAIL USER:",
-  process.env.EMAIL_USER
-);
-
-console.log(
-  "EMAIL PASS EXISTS:",
-  !!process.env.EMAIL_PASS
-);
-
-app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
