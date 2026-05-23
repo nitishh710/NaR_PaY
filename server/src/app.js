5f49d757bb3046edc76da4e83cecab2afd3d0550
nitishh710 <nitishchoudhary5585@gmail.com>
2026-05-23 16:38:17 +0530

    final backend fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,39 +7,20 @@ import transactionRoutes from "./routes/transactionRoutes.js";
 
 const app = express();
 
-app.use(cors({
-    origin: [
-      "http://localhost:5173",
-      "https://narpay-nitishh710s-projects.vercel.app",
-    ],
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-    ],
-    credentials: true,
-  }));
-  
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
-
-app.use(
-  "/api/auth",
-  authRoutes
-);
-
-app.use(
-  "/api/wallet",
-  walletRoutes
-);
-
-app.use(
-  "/api/transactions",
-  transactionRoutes
-);
 
 app.get("/", (req, res) => {
   res.send("NARPAY API RUNNING");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/wallet", walletRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -6,15 +6,19 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 8080;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected");
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
